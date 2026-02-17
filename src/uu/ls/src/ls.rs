37d27e02bea89c3520771f64d9c09c6b3adfd7fa
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-02-17 16:09:16 +0000

    ls: add selinux/smack cfg alias

--- src/uu/ls/src/ls.rs
+++ src/uu/ls/src/ls.rs
@@ -1443,7 +1443,7 @@ fn get_security_context<'a>(
         }
     }
 
-    #[cfg(all(feature = "selinux", any(target_os = "linux", target_os = "android")))]
+    #[cfg(selinux)]
     if config.selinux_supported {
         use uucore::show_warning;
 
@@ -1485,7 +1485,7 @@ fn get_security_context<'a>(
         }
     }
 
-    #[cfg(all(feature = "smack", target_os = "linux"))]
+    #[cfg(smack)]
     if config.smack_supported {
         // For SMACK, use the path to get the label
         // If must_dereference is true, we follow the symlink
